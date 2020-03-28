4e776a384eb9f76e50ac54bddead81c9f1205903
jefer <jefersson290@hotmail.com>
2020-03-28 18:44:25 -0500

    se añade gitignore

---
+++ CursoPOOUber/Java/Uberx.java
@@ -0,0 +1,11 @@
+class Uberx extends Car{
+    String brand;
+    String model;
+
+    public Uberx(String license,Account driver,String brand, String model){
+        super(license, driver);
+        this.brand=brand;
+        this.model=model;
+    }
+
+}
