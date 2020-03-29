602604c4f8e12e06efa72715afa85cd208287753
jefer <jefersson290@hotmail.com>
2020-03-28 19:14:34 -0500

    se añaden herencias

---
+++ CursoPOOUber/Python/uberblack.py
@@ -0,0 +1,9 @@
+from car import Car
+
+class UberBlack(Car):
+    typeCarAccepted=[]
+    seatsMaterial=[]
+    def __init__(self,driver,typeCarAccepted,SeatMaterial):
+        super.__init__(license,driver)
+        self.typeCarAccepted=typeCarAccepted
+        self.seatsMaterial=seatsMaterial
